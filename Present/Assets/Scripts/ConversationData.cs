94fefeb37473850aef2f10a6b0a094b7d2b525f9
tmgarcia <tclustuff@gmail.com>
2018-12-15 17:20:11 -0800

    Conversation data structure

---
+++ Present/Assets/Scripts/ConversationData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "ConversationData", menuName = "Data/ConversationData", order = 1)]
+public class ConversationData : ScriptableObject
+{
+    [SerializeField]
+    private ConversationSegment[] StartingConversationSegments;
+
+    [SerializeField]
+    private ConversationSegment[] RandomConversationSegments;
+}
