c3f13b79ca7db7893d0f0cceb009abcfeffc476c
DESKTOP-N9BDQFK\Dale Chetney <dalechetney@gmail.com>
2018-12-16 23:04:59 -0700

    RunnerEnding

---
+++ Present/Assets/Scripts/EndingHouse.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingHouse : RunnerObject
+{
+    internal override void CollideEffects()
+    {
+        GameManagerScript.Instance.GoToWinScreen();
+    }
+
+    internal override void Despawn()
+    {
+        ObjectPoolService.Instance.ReleaseInstance<EndingHouse>(this);
+    }
+}
