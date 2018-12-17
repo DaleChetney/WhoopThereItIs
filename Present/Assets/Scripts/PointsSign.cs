08b422f3d48fd28d8c0348f21e353609ae523dd3
Luis Andrade <LAndrade1093@gmail.com>
2018-12-16 19:50:29 -0700

    Fixed text issue with single digits

--- Present/Assets/Scripts/PointsSign.cs
+++ Present/Assets/Scripts/PointsSign.cs
@@ -17,6 +17,10 @@ public class PointsSign : MonoSingleton<PointsSign>
 	public void UpdateScoreText(int score)
 	{
 		string text = score.ToString();
+		if(text.Length <= 1)
+		{
+			text = "0" + text;
+		}
 		pointsText.text = text;
 	}
 }
