d8f6339d3d77a2ccf5f433c185f9c3e6b2d3163a
Luis Andrade <LAndrade1093@gmail.com>
2018-12-16 19:45:50 -0700

    Points counter code

---
+++ Present/Assets/Scripts/PointsSign.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PointsSign : MonoSingleton<PointsSign>
+{
+	public Text pointsText;
+	
+	void Update()
+	{
+		int score = GameManagerScript.Instance.Score;
+		UpdateScoreText(score);
+	}
+
+	public void UpdateScoreText(int score)
+	{
+		string text = score.ToString();
+		pointsText.text = text;
+	}
+}
